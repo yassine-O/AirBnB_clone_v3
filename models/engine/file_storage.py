43c525a75809fe4e04d63ecbac1f295b4fb5d761
Yassine <yassine@gmail.com>
2024-06-11 14:39:16 +0000

    2. Improve storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the class and its ID"""
+        key = classes[cls].__name__ + "." + id
+        return self.__objects[key]
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class"""
+        if cls is None:
+            return len(__objects)
+        counter = 0
+        for key in self.__objects.keys():
+            if cls in key:
+                counter += 1
+        return counter
